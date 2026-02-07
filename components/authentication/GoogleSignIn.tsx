d28128c2182f58397a161c4fa465a87e9ca2a80f
Syed Azaan Ahmad <120459599+syazah@users.noreply.github.com>
2026-02-07 14:31:54 +0530

    Trying GoogleOAuth

---
+++ components/authentication/GoogleSignIn.tsx
@@ -0,0 +1,38 @@
+import {
+    GoogleSignin,
+    statusCodes,
+} from "@react-native-google-signin/google-signin";
+
+GoogleSignin.configure({
+    webClientId: "99320420058-sniobca75ssbd1j2r3b54f1s8pvj5esi.apps.googleusercontent.com",
+});
+
+export async function signInWithGoogle() {
+    try {
+        await GoogleSignin.hasPlayServices();
+        const response = await GoogleSignin.signIn();
+        const idToken = response.data?.idToken;
+
+        if (!idToken) {
+            throw new Error("No ID token returned");
+        }
+
+        const res = await fetch("http://YOUR_BACKEND_URL/auth/google", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ idToken }),
+        });
+
+        const data = await res.json();
+        return data;
+    } catch (error: any) {
+        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+            console.log("User cancelled sign-in");
+        } else if (error.code === statusCodes.IN_PROGRESS) {
+            console.log("Sign-in already in progress");
+        } else {
+            console.error("Google sign-in error:", error);
+        }
+        throw error;
+    }
+}
